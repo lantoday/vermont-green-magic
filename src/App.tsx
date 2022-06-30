703c713c5a4f9389fe8b4005f0fe1310b12040b0
Lan Chen <lanchen.it@gmail.com>
2022-06-30 21:32:11 +1200

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Finally it is showing
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vermontgreenmagic.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vermont Green Magic
         </a>
       </header>
     </div>
