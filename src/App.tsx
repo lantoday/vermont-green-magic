8011c85677d5833effb2510388ba6da094677e87
Lan Chen <lanchen.it@gmail.com>
2022-07-04 15:57:18 +1200

    Add 404 file and redirect script, try to fix gh-pages 404 issue

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/varmont-green-magic">
         <NavbarComponent />
         <Routes>
           <Route path="/" element={<HomePageContainer/>} />
