4006c6d5cac4cc363a1ce18a1552cfc5b181ef2d
Lan Chen <lanchen.it@gmail.com>
2022-07-04 16:44:45 +1200

    tiny update for baseURL

--- src/pages/Navbar/NavbarComponent.tsx
+++ src/pages/Navbar/NavbarComponent.tsx
@@ -5,16 +5,17 @@ import Container from 'react-bootstrap/Container';
 import './NavbarComponent.scss'
 
 function NavbarComponent() {
+  var baseURL = "/vermont-green-magic/#/"
   return (
     <>
       <Navbar bg='dark' variant='dark' expand='lg' className='navbar_wrapper'>
         <Container>
-          <Navbar.Brand href='/vermont-green-magic/#/'>Vermont Green Magic</Navbar.Brand>
+          <Navbar.Brand href={baseURL}>Vermont Green Magic</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href='/vermont-green-magic/#/'>Home</Nav.Link>
-              <Nav.Link href='/vermont-green-magic/#/about'>About</Nav.Link>          
+              <Nav.Link href={baseURL}>Home</Nav.Link>
+              <Nav.Link href={baseURL+'about'}>About</Nav.Link>          
             </Nav>
           </Navbar.Collapse>
         </Container>
