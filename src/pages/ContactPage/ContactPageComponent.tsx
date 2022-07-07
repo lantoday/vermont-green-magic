4d44e31f26a0cd38629f23389a60f7d59c01ce68
Lan Chen <lanchen.it@gmail.com>
2022-07-07 21:13:27 +1200

    Contact us v1

--- src/pages/ContactPage/ContactPageComponent.tsx
+++ src/pages/ContactPage/ContactPageComponent.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import contactimg from '../../images/contact/bg.png';
+import { Email } from '../../shared/Icons/Email';
+import { Phone } from '../../shared/Icons/Phone';
 
 export const ContactPageComponent: React.FC = () => (
     <>
-        <p>this is CONTACT US page component</p>
+        <div className="p-2">
+            <p className="page_title">Contact us</p>
+            <div className="d-flex flex-wrap justify-content-center fs-4">
+                <img className="w-50" src={contactimg} alt="Contact us" />
+
+                <div className="d-flex flex-column justify-content-center">
+                    <div className="d-flex p-2">
+                        <p>
+                            <Email />
+                            &nbsp;&nbsp;
+                        </p>
+                        <a
+                            className="fs-4 fw-bold"
+                            href="mailto:someone@yoursite.com"
+                        >
+                            vermontgreenmagic@gmail.com
+                        </a>
+                    </div>
+                    <div className="d-flex">
+                        <p>
+                            <Phone />
+                            &nbsp;&nbsp;
+                        </p>
+                        <p className="fs-4 fw-bold">802-272-9247</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 );
